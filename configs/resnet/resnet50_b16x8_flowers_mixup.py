27808b615be1cecf70f59ca63a4e05e6cb86f165
Tyler Vu <tv3433@princeton.edu>
2024-03-03 01:01:15 +0800

    update epochs -> 100 & resnet(k>34) samples-per-gpu -> 8

--- configs/resnet/resnet50_b16x8_flowers_mixup.py
+++ configs/resnet/resnet50_b16x8_flowers_mixup.py
@@ -1,5 +1,5 @@
 _base_ = [
     '../_base_/models/resnet50_flowers_mixup.py',
-    '../_base_/datasets/flowers_bs32.py',
-    '../_base_/schedules/flowers_bs32.py', '../_base_/default_runtime.py'
+    '../_base_/datasets/flowers_bs8.py',
+    '../_base_/schedules/flowers_bs16.py', '../_base_/default_runtime.py'
 ]
