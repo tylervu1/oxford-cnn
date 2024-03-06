8f69b24fd7848a4b1b629552be3a36638d032441
BuzyPizzaZ <robby42@connect.hku.hk>
2024-03-06 15:07:03 +0800

    trained vgg11 and vgg13, checkpoint save interval=33

--- .ipynb_checkpoints/train_models-checkpoint.sh
+++ .ipynb_checkpoints/train_models-checkpoint.sh
@@ -26,27 +26,27 @@
 #  --checkpoint 'output/resnet50_b16x8_flowers/epoch_99.pth' \
 #  --out 'output/resnet50_b16x8_flowers/test.json'
 
-python tools/train.py \
-  --config 'configs/vgg/vgg11_b32x8_flowers.py' \
-  --work-dir 'output/vgg11_b32x8_flowers'
+#python tools/train.py \
+#  --config 'configs/vgg/vgg11_b32x8_flowers.py' \
+#  --work-dir 'output/vgg11_b32x8_flowers'
 
-python tools/train.py \
-  --config 'configs/vgg/vgg13_b32x8_flowers.py' \
-  --work-dir 'output/vgg13_b32x8_flowers'
+#python tools/train.py \
+#  --config 'configs/vgg/vgg13_b32x8_flowers.py' \
+#  --work-dir 'output/vgg13_b32x8_flowers'
 
 #python tools/train.py \
 #  --config 'configs/vgg/vgg13bn_b32x8_flowers.py' \
 #  --work-dir 'output/vgg13bn_b32x8_flowers'
 
-python tools/test.py \
-  --config 'configs/vgg/vgg11_b32x8_flowers.py' \
-  --checkpoint 'output/vgg11_b32x8_flowers/epoch_99.pth' \
-  --out 'output/vgg11_b32x8_flowers/test.json'
+#python tools/test.py \
+#  --config 'configs/vgg/vgg11_b32x8_flowers.py' \
+#  --checkpoint 'output/vgg11_b32x8_flowers/epoch_99.pth' \
+#  --out 'output/vgg11_b32x8_flowers/test.json'
 
-python tools/test.py \
-  --config 'configs/vgg/vgg13_b32x8_flowers.py' \
-  --checkpoint 'output/vgg13_b32x8_flowers/epoch_99.pth' \
-  --out 'output/vgg13_b32x8_flowers/test.json'
+#python tools/test.py \
+#  --config 'configs/vgg/vgg13_b32x8_flowers.py' \
+#  --checkpoint 'output/vgg13_b32x8_flowers/epoch_99.pth' \
+#  --out 'output/vgg13_b32x8_flowers/test.json'
 
 #python tools/test.py \
 #  --config 'configs/vgg/vgg13bn_b32x8_flowers.py' \
@@ -61,10 +61,21 @@ python tools/test.py \
 ## commands below run into memory issues on 2080 gpu ##
 #######################################################
 
-# python tools/train.py \
+#python tools/train.py \
 #   --config 'configs/resnet/resnet101_b16x8_flowers.py' \
 #   --work-dir 'output/resnet101_b16x8_flowers'
 
-# python tools/train.py \
+#python tools/train.py \
 #   --config 'configs/resnet/resnet152_b16x8_flowers.py' \
 #   --work-dir 'output/resnet152_b16x8_flowers'
+
+#python tools/test.py \
+#   --config 'configs/resnet/resnet101_b16x8_flowers.py' \
+#   --checkpoint 'output/resnet101_b16x8_flowers/epoch_99.pth' \
+#   --out 'output/resnet101_b16x8_flowers/test.json'
+
+#python tools/test.py \
+#   --config 'configs/resnet/resnet152_b16x8_flowers.py' \
+#   --checkpoint 'output/resnet152_b16x8_flowers/epoch_99.pth' \
+#   --out 'output/resnet152_b16x8_flowers/test.json'
+
